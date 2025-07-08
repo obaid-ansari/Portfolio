28291243d23ebadcfea186961edaa9bfebcafb4e
Mohammed saif ansari <ansari.ubaid.1020@gmail.com>
2025-07-08 19:06:08 +0530

    updated part

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,6 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "http://localhost:5174",
-    ],
-  })
-);
 app.use(cors());
 app.use(bodyParser.json());
 
